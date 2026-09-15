cce7b94c6de5df2b4bc617c92bfacf368d4050e7
CaptluckyTiger <mizaelborgo@outlook.com>
2026-09-15 19:02:17 -0300

    fixed github pages

--- src/view/views/HomeView.jsx
+++ src/view/views/HomeView.jsx
@@ -6,7 +6,11 @@ import { useState } from 'react';
 
 export default function HomeView({ store }) {
   const [selected, setSelected] = useState(null);
-  const wallpaper = `url("${import.meta.env.BASE_URL}assets/001.jpg")`;
+  const wallpaperUrl = new window.URL(
+    `${import.meta.env.BASE_URL}assets/001.jpg`,
+    document.baseURI,
+  ).href;
+  const wallpaper = `url("${wallpaperUrl}")`;
   return (
     <>
       <section className="hero" style={{ '--hero-wallpaper': wallpaper }}>
